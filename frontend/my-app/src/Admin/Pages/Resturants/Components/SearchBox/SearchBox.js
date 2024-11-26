364d5ed2f1ef7a18786815049996e0796a07c793
Ebaad Ahmed <ebaadlion@gmail.com>
2024-11-26 22:27:56 +0500

    some little changes

--- frontend/my-app/src/Admin/Pages/Resturants/Components/SearchBox/SearchBox.js
+++ frontend/my-app/src/Admin/Pages/Resturants/Components/SearchBox/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './SearchBox.css'
 const SearchBox = ({searchfield,SearchChange}) => {
     return(
-        <div className = "search_box" >
+        <div className = "search_boxa" >
             <h3 style = {{ fontFamily : "Georgia, serif" }}>Search by Name :</h3>
             <input type="search" 
             placeholder='Search Resturant'
